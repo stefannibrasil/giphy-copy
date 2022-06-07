cd359fa63fefa826cdf506b4c0d3e335ac95d201
Stefanni Brasil <stefannibrasil@gmail.com>
2022-06-06 21:19:25 -0700

    Add unit tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-  has_many_attached :gifs
+  has_many :gifs
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryBot.build(:user) }
+
+  it { expect(FactoryBot.build(:user)).to be_valid }
+
+  describe "associations" do
+    it { should have_many(:gifs) }
+  end
+
+  describe "gifs attachment" do
+    before do
+      @user = FactoryBot.create(:user, :with_gifs)
+    end
+
+    it { expect(@user.gifs.count).to eq 1 }
+  end
 end
